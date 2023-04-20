e24466e198b364ab4ae968671f7369b24e9ca7de
HashTH <bandit.dev@hotmail.com>
2023-04-20 15:57:29 +0700

    in progress

--- unclelab/Mini/Fullfill.py
+++ unclelab/Mini/Fullfill.py
@@ -1,6 +1,28 @@
 from tkinter import *
 from tkinter import ttk
+import subprocess
+import csv
 
+def save_CSV():
+    data = [['รายการ','รายละเอียด','จำนวนเงิน'],
+           [drop1.get(),field1.get(),field2.get()]]
+    with open('moneyTrack.csv','a',newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(data)
+    open_Mini()
+
+def validate_int(value):
+    if value.isdigit():
+        return True
+    elif value == "":
+        return True
+    else:
+        return False
+
+
+def open_Mini():
+    root.destroy()
+    subprocess.Popen(['python', 'Mini.py'])
 
 root = Tk()
 root.title('Money Tracking')
@@ -12,23 +34,43 @@ root.geometry(f'{width}x{height}+{x}+{y}')
 root.config(bg='#FFEDDC')
 
 F1 = Frame(root, width=580, height=660,bg='white')
-FF1 = Frame(F1)
-A1 = Label(FF1,text='รายการ',font=('TH Sarabun New',25))
-A1.grid(row=0)
 
-FF1.pack()
-F1.pack(pady=10)
+FF1 = Frame(F1,bg='white')
+A1 = Label(FF1,text='รายการ',font=('TH Sarabun New',25,'bold'),bg='white')
+A1.grid(row=0,column=0,ipady=10,sticky=W)
+options = ['รายรับ','รายจ่าย']
+selected_option = StringVar()
+drop1 = ttk.Combobox(FF1,font=('TH Sarabun New',20),textvariable=selected_option,values=options)
+drop1.grid(row=0,column=2,ipadx=10,ipady=10)
+FF1.pack(pady=50)
+
+FF2 = Frame(F1,bg='white')
+A2 = Label(FF2,text='รายละเอียด',font=('TH Sarabun New',25,'bold'),bg='white')
+A2.grid(row=0,column=0,ipady=10,sticky=W)
+field1 = Entry(FF2,font=('TH Sarabun New',20))
+field1.grid(row=0,column=2,ipadx=10,ipady=10)
+FF2.pack(pady=50)
+
+FF3 = Frame(F1,bg='white')
+validate_cmd = root.register(validate_int)
+A3 = Label(FF3,text='จำนวนเงิน',font=('TH Sarabun New',25,'bold'),bg='white')
+A3.grid(row=0,column=0,ipady=10,sticky=W)
+field2 = Entry(FF3,font=('TH Sarabun New',20),validate='key',validatecommand=(validate_cmd,'%P'))
+field2.grid(row=0,column=2,ipadx=10,ipady=10)
+FF3.pack(pady=50)
+
+F1.pack(pady=10,padx=10,fill=BOTH,expand=True)
 
 F2 = Frame(root, width=600,height=100,)
 
-B1 = Button(F2,text='บันทึก',font=('TH Sarabun New',20),fg='black',bg='#F0D579',command='')
+B1 = Button(F2,text='บันทึก',font=('TH Sarabun New',20,'bold'),fg='black',bg='#F0D579',command=save_CSV)
 B1.grid(row=0,column=0,ipady=20,ipadx=30,sticky=W)
 Blank1 = Label(F2,text='',bg='#FFEDDC')
 Blank1.grid(row=0,column=1,ipady=40,ipadx=50)
-B2 = Button(F2,text='ยกเลิก',font=('TH Sarabun New',20),fg='black',bg='#F0D579',command='')
+B2 = Button(F2,text='ยกเลิก',font=('TH Sarabun New',20,'bold'),fg='black',bg='#F0D579',command=open_Mini)
 B2.grid(row=0,column=2,ipady=20,ipadx=30,sticky=E)
 
 
-F2.pack()
+F2.pack(pady=10)
 
 root.mainloop()
--- unclelab/Mini/Mini.py
+++ unclelab/Mini/Mini.py
@@ -15,17 +15,17 @@ root.geometry(f'{width}x{height}+{x}+{y}')
 root.config(bg='#FFEDDC')
 
 F1 = Frame(root, width=580, height= 690,bg='white')
-F1.pack(pady=10)
+F1.pack(pady=5,padx=5,fill=BOTH,expand=True)
 
 F2 = Frame(root, width=600,height=100,)
 
-B1 = Button(F2,text='หน้าหลัก',width=17,height=1,font=('TH Sarabun New',20),fg='black',bg='#F0D579',command='')
+B1 = Button(F2,text='หน้าหลัก',width=17,height=1,font=('TH Sarabun New',20,'bold'),fg='black',bg='#F0D579',command='')
 B1.grid(row=0,column=0,ipady=10,ipadx=0)
-B2 = Button(F2,text='บันทึกรายการ',font=('TH Sarabun New',20),fg='black',bg='#F0D579',command=open_add)
+B2 = Button(F2,text='บันทึกรายการ',font=('TH Sarabun New',20,'bold'),fg='black',bg='#F0D579',command=open_add)
 B2.grid(row=0,column=1,ipady=10,ipadx=30)
-B2 = Button(F2,text='รายงาน',font=('TH Sarabun New',20),fg='black',bg='#F0D579',command='')
+B2 = Button(F2,text='รายงาน',font=('TH Sarabun New',20,'bold'),fg='black',bg='#F0D579',command='')
 B2.grid(row=0,column=2,ipady=10,ipadx=50)
 
-F2.pack()
+F2.pack(pady=5)
 
 root.mainloop()
